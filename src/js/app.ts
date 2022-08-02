2d7d2e5956ec6b9c060dabb524d6eefd84668bb3
Ryan Irelan <ryan@mijingo.com>
2022-08-02 11:34:52 -0500

    cleaned up app.ts

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,22 +1,4 @@
-import App from '@/vue/App.vue';
-import {createApp} from 'vue';
-
-// Import our CSS
 import '@/css/app.css';
-
-// App main
-const main = async () => {
-  // Create our vue instance
-  const app = createApp(App);
-
-  // Mount the app
-  return app.mount('#component-container');
-};
-
-// Execute async function
-main().then(() => {
-  console.log();
-});
 
 // Accept HMR as per: https://vitejs.dev/guide/api-hmr.html
 if (import.meta.hot) {
