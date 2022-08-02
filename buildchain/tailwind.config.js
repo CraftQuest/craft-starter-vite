79303cfa5a18411d9544587bd8e6df44d238e557
Ryan Irelan <ryan@mijingo.com>
2022-08-02 16:10:11 -0500

    default to two tailwind plugins and config

--- buildchain/tailwind.config.js
+++ buildchain/tailwind.config.js
@@ -4,8 +4,18 @@ module.exports = {
     '../cms/templates/**/*.{twig,html}',
     '../src/vue/**/*.{vue,html}',
     '../src/css/components/**/*.css',
+    '../src/css/app.css'
   ],
   theme: {
+    extend: {
+      colors: {
+        primary: '#2C5F2D',
+        secondary: '#97BC62FF',
+      }
+    },
   },
-  plugins: [],
-};
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+  ]
+}
